0507edcf2c023c262a79fde4d09ef59dc217cfd4
Sungin Jung <jungs@uchicago.edu>
2024-04-09 22:55:32 -0700

    Create TodoItem model class

---
+++ ToDoAppMaui/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace ToDoAppMaui.Models;
+
+public class TodoItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool Completed { get; set; }
+    public DateTime Due { get; set; }
+}
