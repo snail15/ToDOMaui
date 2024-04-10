0925cd3ed62fe9cf9ba33e0f29679a955cb891d0
Sungin Jung <jungs@uchicago.edu>
2024-04-09 23:45:24 -0700

    implement repository

--- ToDoAppMaui/Models/TodoItem.cs
+++ ToDoAppMaui/Models/TodoItem.cs
@@ -1,8 +1,11 @@
 namespace ToDoAppMaui.Models;
 
+using SQLite;
 public class TodoItem
 {
+    [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
+    
     public string Title { get; set; }
     public bool Completed { get; set; }
     public DateTime Due { get; set; }
--- ToDoAppMaui/Repositories/TodoItemRepository.cs
+++ ToDoAppMaui/Repositories/TodoItemRepository.cs
@@ -1,24 +1,31 @@
-using ToDoAppMaui.Models;
-
 namespace ToDoAppMaui.Repositories;
+
+using ToDoAppMaui.Models;
+using SQLite;
 
 public class TodoItemRepository : ITodoItemRepository
 {
+    private SQLiteAsyncConnection _connection;
 
     public event EventHandler<TodoItem> OnItemAdded;
     public event EventHandler<TodoItem> OnItemUpdated;
     
     public async Task<List<TodoItem>> GetItemsAsync()
     {
-        return null;
+        await CreateConnectionAsync();
+        return await _connection.Table<TodoItem>().ToListAsync();
     }
     public async Task AddItemAsync(TodoItem item)
     {
-        
+        await CreateConnectionAsync();
+        await _connection.InsertAsync(item);
+        OnItemAdded?.Invoke(this, item);
     }
     public async Task UpdateItemAsync(TodoItem item)
     {
-        
+        await CreateConnectionAsync();
+        await _connection.UpdateAsync(item);
+        OnItemUpdated?.Invoke(this, item);
     }
     public async Task AddOrUpdateItemAsync(TodoItem item)
     {
@@ -29,6 +36,28 @@ public class TodoItemRepository : ITodoItemRepository
         else
         {
             await UpdateItemAsync(item);
+        }
+    }
+
+    private async Task CreateConnectionAsync()
+    {
+        if (_connection is not null)
+        {
+            return;
+        }
+        
+        var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        var databasePath = Path.Combine(documentPath, "TodoItems.db");
+        _connection = new SQLiteAsyncConnection(databasePath);
+        await _connection.CreateTableAsync<TodoItem>();
+        
+        if (await _connection.Table<TodoItem>().CountAsync() == 0)
+        {
+            await _connection.InsertAsync(new TodoItem
+            {
+                Title = "Welcome to ToDoAppMaui",
+                Due = DateTime.Now
+            });
         }
     }
 }
